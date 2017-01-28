6c3e5afa0ae028906fdf154025de80d075b3cb81
Eric Yang <eyang232@gmail.com>
2017-01-28 13:13:22 -0800

    Add specs
    Redis must be running locally for specs to run correctly.

--- spec/lita/handlers/count_spec.rb
+++ spec/lita/handlers/count_spec.rb
@@ -3,14 +3,20 @@ require "spec_helper"
 RSpec.describe Lita::Handlers::Regexcellent, :lita_handler => true do
   let(:robot) { Lita::Robot.new(registry) }
 
+
   subject { described_class.new(robot) }
 
   describe "#count" do
-    # it { is_expected.to route("Lita count /regex/") }
+    before do
+      allow_any_instance_of(described_class).to receive(:fetch_slack_message_history).and_return([])
+    end
 
-    # it "responds with a count" do
-    #   send_message("Lita count /regex/")
-    #   expect(replies.last).to match(/(\w+\s) results found\./)
-    # end
+    it { is_expected.to route("Lita count /regex/") }
+    it { is_expected.to route("Lita count /regex/ from:yesterday to:today") }
+
+    it "responds with a count" do
+      send_message("Lita count /regex/")
+      expect(replies.last).to eq "Found 0 results."
+    end
   end
 end
